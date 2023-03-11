10ad33db3886abd0560af3e3267943aed27dec43
ActualPepsiCat <127532411+ActualPepsiCat@users.noreply.github.com>
2023-03-11 22:14:57 +0300

    Update spawnitem.lua

--- spawnitem.lua
+++ spawnitem.lua
@@ -1,4 +1,4 @@
 return function(model)
   local new = LoadCustomInstance(model)
-model:Clone().Parent = game.Players.LocalPlayer.Backpack
+new:Clone().Parent = game.Players.LocalPlayer.Backpack
 end
